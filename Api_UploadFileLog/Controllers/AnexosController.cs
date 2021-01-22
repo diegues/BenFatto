3582eedb633df8bdc1c03b666084a9f84d7eeee1
Michel-Leal <michelg89@gmail.com>
2021-01-22 12:14:45 -0300

    PostFile - Alteração

--- Api_UploadFileLog/Controllers/AnexosController.cs
+++ Api_UploadFileLog/Controllers/AnexosController.cs
@@ -20,6 +20,31 @@ namespace Api_UploadFileLog.Controllers
             _configuration = configuration;
         }
 
+        private string findData(int start, string caracter, ref string linha)
+        {
+            string value = "";
+
+            if (linha.Trim().Length > 0)
+            {
+                int index = linha.Length;
+                int cont = 0;
+
+                if (start > 0)
+                    linha = linha.Substring(start);
+
+                if (linha.IndexOf(caracter) > 0)
+                {
+                    index = linha.IndexOf(caracter);
+                    cont = 1;
+                }
+
+
+                value = linha.Substring(0, index);
+                linha = linha.Substring(value.Length + cont).Trim();
+            }
+            return value.Trim();
+        }
+
         [HttpPost]
         [Route("UploadFile")]
         public string PostFile(IFormFile file)
@@ -31,14 +56,33 @@ namespace Api_UploadFileLog.Controllers
                 using (var reader = new StreamReader(file.OpenReadStream()))
                 {
                     while (reader.Peek() >= 0)
+                    {
+                        sb.Clear();
                         sb.AppendLine(reader.ReadLine());
+
+                        //Fazer Entidade
+
+                        string linha = sb.ToString();
+                        string ip = findData(0, " ", ref linha);
+                        string local = findData(0, " ", ref linha);
+                        string user = findData(0, "[", ref linha);
+                        string data = findData(0, "]", ref linha);
+                        string requisicao = findData(1, "\"", ref linha);
+                        string status = findData(0, " ", ref linha);
+                        string time = findData(0, "\"", ref linha);
+                        string origem = findData(0, "\"", ref linha);
+                        string software = findData(1, "\"", ref linha);
+
+                        //Salvar no banco
+                    }
                 }
             }
-            catch(Exception exc)
+            catch (Exception exc)
             {
-                return  exc.ToString();
+                return exc.ToString();
             }
 
+            //Retornar Json
             return sb.ToString();
         }
     }
